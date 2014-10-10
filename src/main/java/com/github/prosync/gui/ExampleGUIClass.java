961e26aab0b33c587a96f6a61506864eefee95d3
Jim-Espen Gundersen <jimgundersen@gmail.com>
2014-10-10 12:20:59 +0200

    - Add placeholder files in new modules

---
+++ src/main/java/com/github/prosync/gui/ExampleGUIClass.java
@@ -0,0 +1,7 @@
+package com.github.prosync.gui;
+
+/**
+ * Created by jim-espen on 10/10/14.
+ */
+public class ExampleGUIClass {
+}
---
+++ src/main/java/com/github/prosync/logic/ExampleLogicClass.java
@@ -0,0 +1,7 @@
+package com.github.prosync.logic;
+
+/**
+ * Created by jim-espen on 10/10/14.
+ */
+public class ExampleLogicClass {
+}
