e80452e48d8f18aead57b4749335eae2057d0d37
Jim-Espen Gundersen <jimgundersen@gmail.com>
2014-10-16 12:35:48 +0200

    - Add more methods to CameraController

--- src/main/java/com/github/prosync/logic/CameraController.java
+++ src/main/java/com/github/prosync/logic/CameraController.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
  */
 public class CameraController extends Controller {
 	Connector connector;
-	static final ArrayList<String> listOfCommands = new ArrayList<String>(Arrays.asList(new String[]{"PW", "CM", "SH", "VV", "FS", "FV", "BS", "WB", "TI", "CS", "BU", "LO", "PT", "DL", "DA", "AO", "DM", "VM"}));
+	static final ArrayList<String> listOfCommands = new ArrayList<String>(Arrays.asList(new String[]{"PW", "CM", "SH", "VV", "FS", "FV", "BS", "WB", "TI", "CS", "BU", "PT", "DL", "DA", "AO", "DM"}));
 
 	@Override
 	boolean sendCommand(String command, String number) {
@@ -24,6 +24,146 @@ public class CameraController extends Controller {
 			e.printStackTrace();
 		}
 		return true;
+	}
+
+	@Override
+	void turnGoProOn() {
+		sendCommand("PW", "01");
+	}
+
+	@Override
+	void turnGoProOff() {
+		sendCommand("PW", "00");
+	}
+
+	@Override
+	void setAutoOffNever() {
+		sendCommand("AO", "00");
+	}
+
+	@Override
+	void setAutoOff60s() {
+		sendCommand("AO", "01");
+	}
+
+	@Override
+	void setAutoOff120s() {
+		sendCommand("AO", "02");
+	}
+
+	@Override
+	void setAutoOff300s() {
+		sendCommand("AO", "03");
+	}
+
+	@Override
+	void setDefaultModeVideo() {
+		sendCommand("DM", "00");
+	}
+
+	@Override
+	void setDefaultModePhoto() {
+		sendCommand("DM", "01");
+	}
+
+	@Override
+	void setDefaultModeBurst() {
+		sendCommand("DM", "02");
+	}
+
+	@Override
+	void setDefaultModeTimelapse() {
+		sendCommand("DM", "03");
+	}
+
+	@Override
+	void setModeToVideo() {
+		sendCommand("CM", "00");
+	}
+
+	@Override
+	void setModeToPhoto() {
+		sendCommand("CM", "01");
+	}
+
+	@Override
+	void setModeToBurst() {
+		sendCommand("CM", "02");
+	}
+
+	@Override
+	void setModeToTimelapse() {
+		sendCommand("CM", "03");
+	}
+
+	@Override
+	void startCamera() {
+		sendCommand("SH", "01");
+	}
+
+	@Override
+	void stopCamera() {
+		sendCommand("SH", "00");
+	}
+
+	@Override
+	void setWideFoV() {
+		sendCommand("FV", "00");
+	}
+
+	@Override
+	void setMediumFoV() {
+		sendCommand("FV", "01");
+	}
+
+	@Override
+	void setNarrowFoV() {
+		sendCommand("FV", "02");
+	}
+
+	@Override
+	void setVolumeOff() {
+		sendCommand("BS", "00");
+	}
+
+	@Override
+	void setVolume70() {
+		sendCommand("BS", "01");
+	}
+
+	@Override
+	void setVolume100() {
+		sendCommand("BS", "02");
+	}
+
+	@Override
+	void setContinuousShotSingle() {
+		sendCommand("CS", "00");
+	}
+
+	@Override
+	void setContinuousShot3SPS() {
+		sendCommand("CS", "03");
+	}
+
+	@Override
+	void setContinuousShot5SPS() {
+		sendCommand("CS", "05");
+	}
+
+	@Override
+	void setContinuousShot10SPS() {
+		sendCommand("CS", "0a");
+	}
+
+	@Override
+	void setProTuneOn() {
+		sendCommand("PT", "01");
+	}
+
+	@Override
+	void setProTuneOff() {
+		sendCommand("PT", "00");
 	}
 
 }
