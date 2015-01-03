5c87f468fb8ae0fbe05b93cfbbb546602dcf1103
oeystein <huseby@live.com>
2015-01-03 14:03:08 +0100

    Renamed some norwegian variables to english

--- src/main/java/com/github/prosync/gui/ChooseCameraMenu.java
+++ src/main/java/com/github/prosync/gui/ChooseCameraMenu.java
@@ -7,11 +7,11 @@ import java.awt.*;
  * Created by jim-espen on 10/17/14.
  */
 public class ChooseCameraMenu extends JFrame {
-	private JCheckBox kamera1CheckBox;
-	private JCheckBox kamera2CheckBox;
-	private JCheckBox kamera3CheckBox;
-	private JButton tilbakeButton;
-	private JButton hovedmenyButton;
+	private JCheckBox camera1CheckBox;
+	private JCheckBox camera2CheckBox;
+	private JCheckBox camera3CheckBox;
+	private JButton backButton;
+	private JButton mainMenuButton;
 	private JButton okButton;
 
 	public ChooseCameraMenu(String title) {
@@ -19,11 +19,11 @@ public class ChooseCameraMenu extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		setLayout(new GridBagLayout());
-		add(kamera1CheckBox);
-		add(kamera2CheckBox);
-		add(kamera3CheckBox);
-		add(tilbakeButton);
-		add(hovedmenyButton);
+		add(camera1CheckBox);
+		add(camera2CheckBox);
+		add(camera3CheckBox);
+		add(backButton);
+		add(mainMenuButton);
 		add(okButton);
 	}
 }
